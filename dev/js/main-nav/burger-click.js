9c392fd8b1e43fe405354dd2f6549e042c651f34
CatBatchelder <63326220+CatBatchelder@users.noreply.github.com>
2020-07-20 17:21:47 -0400

    trying to add nav

---
+++ dev/js/main-nav/burger-click.js
@@ -0,0 +1,7 @@
+//adding a listener the burger that will call a function that is defined in the nav-global js file
+
+// $("#burger").on("click", hideShowMainNav);
+
+$("#burger").on("click", function(){
+    console.log("clicked on burger");
+});
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,6 +1,10 @@
 // @codekit-prepend "fancybox.js";
 // @codekit-prepend "tinyslider.js";
 
+// @codekit-prepend "main-nav/nav-global.js";
+// @codekit-prepend "main-nav/burger-click.js";
+// @codekit-prepend "main-nav/main-nav-click.js";
+
 // A $( document ).ready() block.
 $( document ).ready(function() {
     console.log( "change" );
