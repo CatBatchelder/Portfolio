0b14cfb47d0e22a0621c54efc2a866b59a882f92
CatBatchelder <63326220+CatBatchelder@users.noreply.github.com>
2020-07-24 23:01:43 -0400

    updated navigation to drop in

---
+++ dev/js/main-nav/burger-animation.js
@@ -0,0 +1,29 @@
+var burgerAnimationTimeline = gsap.timeline({paused:true});
+
+//setting a universal speed for the burger animation
+var burgerAnimationSpeed = .25;
+
+// making the rotation come from the center
+gsap.set(".lines",{transformOrigin:"center"});
+
+// animate the middle line out, rotate top and bottom lines
+burgerAnimationTimeline.to("#middle-line",{duration:burgerAnimationSpeed, alpha:0},"burgerStart")
+                        .to("#top-line",{duration:burgerAnimationSpeed, rotation:45, y:10, stroke:"#6A8AA7"},"burgerStart")
+                        .to("#bottom-line",{duration:burgerAnimationSpeed, rotation:-45, y:-10, stroke:"#6A8AA7", onReverseComplete:resetBurgerLineColor},"burgerStart");
+
+
+function animateBurger(){
+    // check the  canYouSeeTheMenu bool valus
+    if(canYouSeeTheMenu === true){
+        // turn the burger into x and play timeline
+        burgerAnimationTimeline.play();
+    }else{
+        // turn the x back into a burger
+        burgerAnimationTimeline.reverse();
+    }
+}
+
+//create function for lines to go back to normal color
+function resetBurgerLineColor(){
+    gsap.to(".lines",{duration:0.25, stroke:"#fff"});
+}
--- dev/js/main-nav/nav-global.js
+++ dev/js/main-nav/nav-global.js
@@ -1,5 +1,40 @@
-function hideShowMainNav(){
-    console.log("hide or show nav");
+var canYouSeeTheMenu = false;
+
+var navHeight = $("#main-nav").outerHeight();
+//move the main nav off screen 
+gsap.set("#main-nav", {
+    y: -navHeight
+})
+
+//define the drop in nav variable
+var mainNavTimeline = gsap.timeline({
+    paused: true
+})
+
+//make the nav play
+mainNavTimeline.to("#main-nav", {
+    duration: 1,
+    y: 0
+})
+
+function hideShowMainNav() {
+    //console.log("hide or show nav");
     //toggles the css display property
-    $("#main-nav").toggle();
+    //$("#main-nav").toggle();
+
+    if (canYouSeeTheMenu === false) {
+        console.log("show me the menu")
+        canYouSeeTheMenu = true;
+        //turn the burger into x
+        animateBurger();
+        //play the drop in navigation
+        mainNavTimeline.play()
+    } else {
+        console.log("hide the menu")
+        canYouSeeTheMenu = false;
+        //turn the x into burger
+        animateBurger();
+        //get rid of drop in navigation
+        mainNavTimeline.reverse()
+    }
 }
