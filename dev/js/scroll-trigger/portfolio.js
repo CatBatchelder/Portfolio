6b719a4817d8662ae4948ac7b27760dcafdae1bf
CatBatchelder <63326220+CatBatchelder@users.noreply.github.com>
2020-08-10 18:12:33 -0400

    added changes

--- dev/js/scroll-trigger/portfolio.js
+++ dev/js/scroll-trigger/portfolio.js
@@ -2,22 +2,22 @@
     Paralax Pinning
 ======================== */
 
-gsap.to(".portfolio-heros",{
-    // yPercent: -100, 
-    // ease: "none",
-    //stagger: 0.5,
+// gsap.to(".portfolio-heros",{
+//     // yPercent: -100, 
+//     // ease: "none",
+//     //stagger: 0.5,
     
-    scrollTrigger:{
-    trigger:"#portfolio-img",
-    pin: true,
-    pinSpacing: false,
-    markers:true,
-    // start: "top 50",
-    // end: "bottom 50"
-    start: "top top",
-      end: "+=300%"
+//     scrollTrigger:{
+//     trigger:"#portfolio-img",
+//     pin: true,
+//     pinSpacing: false,
+//     markers:true,
+//     // start: "top 50",
+//     // end: "bottom 50"
+//     start: "top top",
+//       end: "+=300%"
 
-}})
+// }})
 
 // gsap.to(".panel:not(:last-child)", {
 //     yPercent: -100, 
